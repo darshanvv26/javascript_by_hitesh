f16bfb52e26de8435de214c0cba3634a655e48a3
darshanvv26 <darshanvv26@gmail.com>
2025-02-03 08:47:37 +0530

    add more about scopes

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -5,11 +5,54 @@ var c = 30  //we need to avoid using this var because it causes problems of scop
 if(true){
     let a = 20
     const b = 30
-    console.log("Inner: ", a, b);
+    // console.log("Inner: ", a, b);
     
 }  //this is block level scope whatever declared here stays here 
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function one(){
+    let userName = "Darshan"
+    function two(){
+        let website = "youtube"
+        console.log("MyName:", userName);
+    }
+    // console.log(website);  //this line causes error because we can not access the variabe declared in inner block scope
+
+    two()
+}
+
+// one()
+
+if(true){
+    let userName = "Darshan"
+    if(userName == "Darshan"){
+        let website = " youtube"
+        console.log(userName + website);
+        
+    }
+    // console.log(website); //same as explained above this causes error 
+    
+}
+
+// console.log(userName); //we can not access this userName variable
+
+
+//++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addOne(8))  //this is normal function declaration we can call function from wherever we want
+function addOne(num){
+    return num + 1
+}
+        
+
+const addTwo = function(num){  //this is also a type of function declaration also called as expression we can also learn about function hosting
+    return num + 2
+}
+
+console.log(addTwo(8))
